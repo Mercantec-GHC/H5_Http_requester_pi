99391193a6ff1bc5f61be5a025dd3269a29f020a
krixzy <mortensenkim@msn.com>
2026-04-01 15:41:36 +0200

    Release 0.2.0 improvements
    Added authentication to API calls (closes #40)
    
    - Added automatic reconnect handling to the MQTT broker (closes #39)
    - Fixed issue where curl did not follow redirects, causing 301 responses (closes #38)
    - Added worker to immediately ping new pages when they are created (closes #47)

--- http_requester/lib/service/faraday_service.rb
+++ http_requester/lib/service/faraday_service.rb
@@ -1,16 +1,29 @@
 class FaradayService
   attr_accessor :destanation_path
 
-  def self.get_response(destanation_path)
-    new(destanation_path).get_response
+  def self.get_bearer_token(destanation_path)
+    new(destanation_path).get_bearer_token
   end
 
+  def self.get_response(destanation_path, token)
+    new(destanation_path).get_response(token)
+  end
+  
   def initialize(destanation_path)
     @destanation_path = destanation_path
   end
   
-  def get_response
-    faraday_client.get(destanation_path)
+  def get_response(token)
+    faraday_client.get(destanation_path) do |req|
+      req.headers['Authorization'] = "Bearer #{token}"
+    end
+  end
+
+  def get_bearer_token
+    faraday_client.post(destanation_path) do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = {email: ENV.fetch("ADMIN_EMAIL"), password: ENV.fetch("ADMIN_PASSWORD")}.to_json
+    end
   end
 
   private
